4e8dcb43736153ada2b940bee387738a424a39e4
tieubochet <tao.nguyen712@gmail.com>
2025-10-03 14:37:02 +0700

    update func login

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ReownAppKitProvider } from "@reown/appkit";
-import '@reown/appkit/styles';
 
 const config = {
   appName: "Be a Celorian",
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path' // Thêm dòng này
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: { // Thêm khối này
-    alias: {
-      '@reown/appkit/styles': path.resolve(__dirname, 'node_modules/@reown/appkit/dist/styles.css'),
-    }
-  }
 })
